1a714e58a3d79cc99280d2aa9ae07ccb019ee740
patricklongo1 <ck.longo@bol.com.br>
2020-04-24 15:16:26 -0300

    unform and input register

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+import { Form } from '@unform/web';
 
 import logo from '../../assets/logo.svg';
 
@@ -9,12 +10,17 @@ import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
 const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
+
   return (
     <Container>
       <Background />
       <Content>
         <img src={logo} alt="GoBarber" />
-        <form>
+
+        <Form onSubmit={handleSubmit}>
           <h1>Faça seu Cadastro</h1>
           <Input name="name" icon={FiUser} placeholder="Nome" />
           <Input name="email" icon={FiMail} placeholder="E-mail" />
@@ -25,7 +31,7 @@ const SignUp: React.FC = () => {
             placeholder="Senha"
           />
           <Button type="submit">Cadastrar</Button>
-        </form>
+        </Form>
 
         <a href="register">
           <FiArrowLeft />
