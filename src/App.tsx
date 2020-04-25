4d8c3ba28d660ec9276d36fc429540f7f5706dba
patricklongo1 <ck.longo@bol.com.br>
2020-04-24 21:46:50 -0300

    AuthContext and API working

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import GlobalStyle from './styles/global';
 
+import { AuthProvider } from './context/AuthContext';
+
 import SignIn from './pages/SignIn';
 /* import SignUp from './pages/SignUp'; */
 
 const App: React.FC = () => (
   <>
-    <SignIn />
     <GlobalStyle />
+    <AuthProvider>
+      <SignIn />
+    </AuthProvider>
   </>
 );
 
